214364fa383cec2776a19e401aa15382b0ec569b
Lauren Frankovich <frankovister@gmail.com>
2020-05-27 22:33:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ def reverse_each_word(string)
   array = string.split
   new_array = []
   array.collect do |string|
-    new_array << string.reverse
+    new_array.push(string.reverse)
   end
   new_array.join(' ')
 end
